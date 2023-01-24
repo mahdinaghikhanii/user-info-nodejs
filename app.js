445de07231ac540c0b887abc11ee222f40f92acd
Mahdinaghikhani <mahdinkh7@gmail.com>
2023-01-24 19:02:28 +0330

    Create local data base

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+const { json } = require('express');
 const http = require('http');
 
 
-http.createServer((req , res)=> {
-    console.log("req is here");
+let database = {
+   users : [
+    {id : 1 , username : 'mahdi' , email : "mahdi@gmail.com" ,password : "123456"},
+    {id : 2 , username : 'ali' , email : "alii@gmail.com" ,password : "123456"},
+    {id : 3 , username : 'amin' , email : "amin@gmail.com" ,password : "123456"},
+   ],
+   courses : [
+    {id : 1 , title : 'Flutter' , price : "2000"},
+    {id : 1 , title : 'JS' , price : "7000"},
+    {id : 1 , title : 'Node' , price : "5000"},
+   ]
+}
+
+
+const server =  http.createServer((req , res)=> {
+    console.log(req.url);
+    if(req.url == 'api/users'){
+        let users = database.users;
+        res.write(json(users));
+    }
     res.end();
-}).listen(3000);
+});
+
+server.listen(3000);
